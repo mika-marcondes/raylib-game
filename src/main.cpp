59865f3b94b05f30bd3b6c8191a8a5f1dcfd2d14
Mikael <mika.marcondes2@gmail.com>
2024-10-17 14:59:38 -0300

    flip player sprite

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "rlgl.h"
 
 int main()
 {
@@ -8,36 +9,54 @@ int main()
     InitWindow(screenWidth, screenHeight, "Hello World");
     // Load textures
     Texture2D player = LoadTexture("../assets/human/idle/base_idle_strip9.png");
-    Rectangle frameRec = {0.0f, 0.0f, (float)player.width / 9, (float)player.height};
-    Vector2 playerPos = {350.0f, 300.0f};
+    SetTextureFilter(player, RL_TEXTURE_FILTER_NEAREST);
+    int playerFrameWidth = player.width / 9;
+    int playerFrameHeight = player.height;
+    Vector2 playerPosition = {350.0f, 300.0f};
+    Rectangle frameRec = {0.0f, 0.0f, (float)playerFrameWidth, (float)playerFrameHeight};
 
     float playerSpeed = 4;
     int framesCounter = 0;
     int framesSpeed = 10;
     int currentFrame = 0;
 
-
     Camera2D camera = {0};
-    camera.target = (Vector2){playerPos.x + 20.0f, playerPos.y + 20.0f};
+    camera.target = (Vector2){playerPosition.x + 20.0f, playerPosition.y + 20.0f};
     camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
-    camera.zoom = 1.0f;
+    camera.zoom = 2.0f;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        //update
-        if (IsKeyDown(KEY_RIGHT)) playerPos.x += playerSpeed;
-        if (IsKeyDown(KEY_LEFT)) playerPos.x -= playerSpeed;
-        if (IsKeyDown(KEY_UP)) playerPos.y -= playerSpeed;
-        if (IsKeyDown(KEY_DOWN)) playerPos.y += playerSpeed;
+        // Update
+        if (IsKeyDown(KEY_D))
+        {
+            playerPosition.x += playerSpeed;
+            if (frameRec.width < 0)
+            {
+                frameRec.width = -frameRec.width;
+            }
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            playerPosition.x -= playerSpeed;
+            if (frameRec.width > 0)
+            {
+                frameRec.width = -frameRec.width;
+            }
+        }
 
-        if (IsKeyPressed(KEY_Q)) camera.zoom += 0.5f;
-        if (IsKeyPressed(KEY_E)) camera.zoom -= 0.5f;
+        if (IsKeyDown(KEY_W)) playerPosition.y -= playerSpeed;
+        if (IsKeyDown(KEY_S)) playerPosition.y += playerSpeed;
+
+        if (IsKeyPressed(KEY_Q)) camera.zoom += 1.0f;
+        if (IsKeyPressed(KEY_E)) camera.zoom -= 1.0f;
         if (IsKeyPressed(KEY_R)) camera.zoom = 1.0f;
 
-        camera.target = (Vector2){playerPos.x + 20.0f, playerPos.y + 20.0f};
+
+        camera.target = (Vector2){playerPosition.x + 20.0f, playerPosition.y + 20.0f};
         if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-        if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+        if (camera.zoom < 1.0f) camera.zoom = 1.0f;
 
         framesCounter++;
         if (framesCounter >= 60 / framesSpeed)
@@ -45,21 +64,21 @@ int main()
             framesCounter = 0;
             currentFrame++;
             if (currentFrame > 8) currentFrame = 0;
-            frameRec.x = (float)currentFrame * (float)player.width / 9;
+            frameRec.x = (float)currentFrame * (float)playerFrameWidth;
         }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
         BeginMode2D(camera);
-        DrawTextureRec(player, frameRec, playerPos, WHITE);
+        DrawTextureRec(player, frameRec, playerPosition, WHITE);
         DrawText("Hello World!", 350, 300, 20, LIGHTGRAY);
         EndMode2D();
-        DrawFPS(10, 10);
 
+        DrawFPS(10, 10);
         EndDrawing();
     }
-
 
     // De-Initialization
     UnloadTexture(player);
