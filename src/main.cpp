8a6d65628a9235f325b4e4b6d8cc8876e7216b1c
Mikael <mika.marcondes2@gmail.com>
2024-10-17 13:17:27 -0300

    add player idle animation

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,37 @@ int main()
     Rectangle frameRec = {0.0f, 0.0f, (float)player.width / 9, (float)player.height};
     Vector2 position = {350.0f, 300.0f};
 
+    int framesCounter = 0;
+    int framesSpeed = 10;
+    int currentFrame = 0;
+
     SetTargetFPS(60);
 
 
     while (!WindowShouldClose())
     {
         //update
-
+        framesCounter++;
+        if (framesCounter >= 60 / framesSpeed)
+        {
+            framesCounter = 0;
+            currentFrame++;
+            if (currentFrame > 8) currentFrame = 0;
+            frameRec.x = (float)currentFrame * (float)player.width / 9;
+        }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawTexture(player, position.x, position.y, WHITE);
+        DrawTextureRec(player, frameRec, position, WHITE);
         DrawText("Hello World!", 350, 300, 20, LIGHTGRAY);
         DrawFPS(10, 10);
 
         EndDrawing();
     }
+
+    // De-Initialization
+    UnloadTexture(player);
 
     CloseWindow();
     return 0;
