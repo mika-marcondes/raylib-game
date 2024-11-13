41a1d8b8c626c06308c5578d2b12ded433b66ef0
mikael <mika.marcondes2@gmail.com>
2024-11-13 20:02:10 -0300

    i mean, he does walk

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,65 @@
 #include "raylib.h"
 
-typedef struct Player
-{
-    Vector2 position;
-    Vector2 speed;
-    Color color;
-    Texture2D texture;
-
-    int frameSize;
-    int frameCurrent;
-    Rectangle frameRec;
-} Player;
-
+#include <random>
 
 static constexpr int screenWidth = 800;
 static constexpr int screenHeight = 600;
 
-static int frameCounter = 0;
-static int currentFrame = 0;
-static int frameSpeed = 15;
-static float playerSpeed = 4.0f;
+class Walker {
+public:
+  int x;
+  int y;
 
-static Player player = {0};
-static Camera2D camera = {0};
+  void Setup() {
+    this->x = screenWidth / 2;
+    this->y = screenHeight / 2;
+  }
 
-void InitCamera()
-{
-    camera.target = (Vector2){player.position.x + 20.0f, player.position.y + 20.0f};
-    camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
-    camera.zoom = 2.0f;
-}
+  void Step() {
+    int min = 0;
+    int max = 3;
 
-void InitPlayer(Texture2D texture, const int frameSize)
-{
-    player.texture = texture;
-    player.position = Vector2(350.0f, 300.0f);
-    player.frameSize = frameSize;
-    player.frameCurrent = player.texture.width / player.frameSize;
-    player.frameRec = {0.0f, 0.0f, (float)player.frameCurrent, (float)player.texture.height};
-    player.color = WHITE;
-}
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution distrib(min, max);
 
-void UpdateTexture(Texture2D texture, const int frameSize)
-{
-    player.texture = texture;
-    player.frameSize = frameSize;
-}
+    int choice = distrib(gen);
 
-int main()
-{
-    // Initialization
-    InitWindow(screenWidth, screenHeight, "Hello World");
-    Texture2D animRun = LoadTexture("../resources/human/base_run_strip8.png");
-    Texture2D animIdle = LoadTexture("../resources/human/base_idle_strip9.png");
-    InitPlayer(animIdle, 9);
-    InitCamera();
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        // Update
-        if (IsKeyDown(KEY_RIGHT))
-        {
-            player.position.x += playerSpeed;
-            UpdateTexture(animRun, 8);
-            if (player.frameRec.width < 0)
-            {
-                player.frameRec.width = -player.frameRec.width;
-            }
-        }
-        if (IsKeyReleased(KEY_RIGHT))
-        {
-            UpdateTexture(animIdle, 9);
-        }
-        if (IsKeyDown(KEY_LEFT))
-        {
-            player.position.x -= playerSpeed;
-            UpdateTexture(animRun, 8);
-            if (player.frameRec.width > 0)
-            {
-                player.frameRec.width = -player.frameRec.width;
-            }
-        }
-        if (IsKeyReleased(KEY_LEFT))
-        {
-            UpdateTexture(animIdle, 9);
-        }
-        if (IsKeyDown(KEY_UP))
-        {
-            player.position.y -= playerSpeed;
-            UpdateTexture(animRun, 8);
-        }
-        if (IsKeyReleased(KEY_UP))
-        {
-            UpdateTexture(animIdle, 9);
-        }
-        if (IsKeyDown(KEY_DOWN))
-        {
-            player.position.y += playerSpeed;
-            UpdateTexture(animRun, 8);
-        }
-        if (IsKeyReleased(KEY_DOWN))
-        {
-            UpdateTexture(animIdle, 9);
-        }
-
-        if (IsKeyPressed(KEY_Q)) camera.zoom += 1.0f;
-        if (IsKeyPressed(KEY_E)) camera.zoom -= 1.0f;
-        if (IsKeyPressed(KEY_R)) camera.zoom = 1.0f;
-
-        camera.target = (Vector2){player.position.x + 20.0f, player.position.y + 20.0f};
-        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-        if (camera.zoom < 1.0f) camera.zoom = 1.0f;
-
-        frameCounter++;
-        if (frameCounter >= 60 / frameSpeed)
-        {
-            frameCounter = 0;
-            currentFrame++;
-            if (currentFrame > player.frameSize - 1) currentFrame = 0;
-            player.frameRec.x = (float)currentFrame * (float)player.frameCurrent;
-        }
-
-        // Draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        BeginMode2D(camera);
-        DrawTextureRec(player.texture, player.frameRec, player.position, player.color);
-        DrawText("Hello World!", 350, 300, 20, LIGHTGRAY);
-        EndMode2D();
-
-        DrawFPS(10, 10);
-        EndDrawing();
+    if (choice == 0) {
+      this->x++;
+    } else if (choice == 1) {
+      this->x--;
+    } else if (choice == 2) {
+      this->y++;
+    } else {
+      this->y--;
     }
-    // De-Initialization
-    UnloadTexture(animIdle);
-    UnloadTexture(animRun);
+  }
 
-    CloseWindow();
-    return 0;
+  void Draw() const { DrawCircle(this->x, this->y, 20, BLACK); }
+};
+
+int main() {
+  // Initialization
+  Walker walker;
+  InitWindow(screenWidth, screenHeight, "Hello World");
+  walker.Setup();
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    // Update
+    walker.Step();
+    // Draw
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    walker.Draw();
+    DrawText("Hello World!", 350, 300, 20, LIGHTGRAY);
+    EndMode2D();
+
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+  // De-Initialization
+  CloseWindow();
+  return 0;
 }
